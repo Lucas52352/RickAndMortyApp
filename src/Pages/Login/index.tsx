621ed3dc6ea2a25f180a7f5f116dbeb6778c8245
Lucas52352 <lucasrius86@gmail.com>
2023-06-09 18:01:50 -0300

    Login

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,20 +1,83 @@
-import { Container, Grid, Paper } from "@mui/material";
-import React from "react";
+import { Box, Button, Container, Grid, Paper, TextField, Typography } from "@mui/material";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
 export const LoginPage: React.FC<{}> = () => {
+
+    type LoginType = {
+        username: string,
+        password: string
+    }
+
+    const [loginData, setLoginData] = useState<LoginType>({
+        username: '',
+        password: ''
+    })
+
+    const dataLogin = (event: ChangeEvent<HTMLInputElement>) => {
+        setLoginData({...loginData, [event.target.name]: event.target.value})
+     
+    }
+
+    const handleSubmit = (event: FormEvent<HTMLInputElement>) => {
+        event.preventDefault()
+        console.log(loginData);
+        
+    }
+
     return (
         <div>
-            <Container maxWidth='xl'>
+            <Container maxWidth='sm'>
                 <Grid 
                     container
-                    direction='row'
+                    direction='column'
                     alignItems='center'
                     justifyContent='center'
-                    sx={{minHeight: '100vh'}}
+                    sx={{minHeight: '90vh'}}
                 >
+
                     <Grid item >
-                        <Paper sx={{padding: '2em'}}>aaa</Paper>  
+
+                        <Paper sx={{padding: '1.2em', borderRadius:'.5em'}}>
+
+                            <Typography variant="h5" sx={{mt: 1, mb: 1}}>Log In</Typography>
+                            <Box component='form' onSubmit={handleSubmit}>
+
+                                    <TextField 
+                                        onChange={dataLogin}
+                                        name="username"
+                                        margin='normal'
+                                        type="text"
+                                        fullWidth 
+                                        label='Username' 
+                                        sx={{mt: 2, mb: 1.5}} 
+                                        required
+                                    />
+
+                                    <TextField 
+                                        onChange={dataLogin}
+                                        name="password"
+                                        margin='normal'
+                                        type="password"
+                                        fullWidth 
+                                        label='Password' 
+                                        sx={{mt: 1.5, mb: 1.5}} 
+                                        required
+                                    />
+
+                                    <Button 
+                                        fullWidth 
+                                        type="submit" 
+                                        variant="contained" 
+                                        sx={{mt: 1.5, mb: 3}}
+                                    >
+                                        Log In
+                                    </Button>
+                            </Box>    
+
+                        </Paper>  
+
                     </Grid>
+
                 </Grid>
             </Container>
         </div>
